d6d42fe05b7fe70330627bac4e18ed33769d3fef
Techie Joe <185759893+techie-joe@users.noreply.github.com>
2026-03-16 00:19:28 +0800

    update worker

--- worker.js
+++ worker.js
@@ -1,5 +1,26 @@
 export default {
   async fetch(request, env) {
-    return new Response("Worker is running")
+    // return new Response("Worker is running")
+
+    const url = new URL(request.url)
+    const parts = url.pathname.split("/")
+
+    if (parts.length < 3) {
+      return new Response("Use /user/ID")
+    }
+
+    const user = parts[2]
+    const selections = parts[3]
+    const api = `https://api.torn.com/user/${user}?key=${env.TORN_API_KEY}&selections=${selections}`
+
+    const response = await fetch(api)
+    const data = await response.text()
+
+    return new Response(data, {
+      headers: {
+        "content-type": "application/json"
+      }
+    })
+
   }
 }
