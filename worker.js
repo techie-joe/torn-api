deabb0f2f5566f1841386ca64fd150a1dc951c61
Techie Joe <185759893+techie-joe@users.noreply.github.com>
2026-03-16 01:07:55 +0800

    update v1 api

--- worker.js
+++ worker.js
@@ -3,22 +3,34 @@ export default {
     // return new Response("Worker is running")
 
     const url = new URL(request.url)
-    const parts = url.pathname.split("/")
+    const parts = url.pathname.split("/").filter(Boolean)
 
-    if (parts.length < 3) {
-      return new Response("Use /user/ID")
+    const section = parts[0]
+    const id = parts[1]
+    const selections = parts[2]
+
+    if (!section) {
+      return new Response("Use /{section}/{id}/{selections}", { status: 400 })
+    }
+    else if (section === "user" && !id) {
+      return new Response("Use /user/{id}/{selections}", { status: 400 })
     }
 
-    const user = parts[2]
-    const selections = parts[3]
-    const api = `https://api.torn.com/user/${user}?key=${env.TORN_API_KEY}&selections=${selections}`
+    const api = new URL(`https://api.torn.com/${section}/${id}`)
+    api.searchParams.set("key", env.TORN_API_KEY)
+    api.searchParams.set("comment", 'TORN_API')
+
+    if (selections) {
+      api.searchParams.set("selections", selections)
+    }
 
     const response = await fetch(api)
-    const data = await response.text()
+    const data = await response.json()
 
-    return new Response(data, {
+    return new Response(JSON.stringify(data), {
       headers: {
-        "content-type": "application/json"
+        "content-type": "application/json",
+        // "cache-control": "max-age=60"
       }
     })
 
