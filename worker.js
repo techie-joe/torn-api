502e2f5cec2f69976b49681d769d6f1e0089f19d
Techie Joe <185759893+techie-joe@users.noreply.github.com>
2026-03-16 11:02:24 +0800

    update routing

--- worker.js
+++ worker.js
@@ -1,38 +1,74 @@
-export default {
-  async fetch(request, env) {
-    // return new Response("Worker is running")
-
-    const url = new URL(request.url)
-    const parts = url.pathname.split("/").filter(Boolean)
-
-    const section = parts[0]
-    const id = parts[1]
-    const selections = parts[2]
-
+// worker.js
+const response_headers = {
+  "cache-control": "max-age=60" // 60 sec
+};
+const response = {
+  help: ()=>{
+    return new Response([
+      "Use /{route}/{section}/{id}/{selections}"
+    ].join('\n'), { status: 400 });
+  },
+  v1: async (parts,env)=>{
+    const route = parts[0];
+    const section = parts[1];
+    const id = parts[2];
+    const selections = parts[3];
     if (!section) {
-      return new Response("Use /{section}/{id}/{selections}", { status: 400 })
+      return new Response([
+        `Use /${route}/{section}/{id}/{selections}`
+      ].join('\n'), { status: 400 });
+    } else if (!id) {
+      return new Response([
+        `Use /${route}/${section}/{id}/{selections}`
+      ].join('\n'), { status: 400 });
     }
-    else if (section === "user" && !id) {
-      return new Response("Use /user/{id}/{selections}", { status: 400 })
-    }
-
-    const api = new URL(`https://api.torn.com/${section}/${id}`)
-    api.searchParams.set("key", env.TORN_API_KEY)
-    api.searchParams.set("comment", 'TORN_API')
-
+    const api = new URL(`https://api.torn.com/${section}/${id}`);
+    api.searchParams.set("key", env.TORN_API_KEY);
+    api.searchParams.set("comment", "TORN_API");
     if (selections) {
-      api.searchParams.set("selections", selections)
+      api.searchParams.set("selections", selections);
     }
-
-    const response = await fetch(api)
-    const data = await response.json()
-
+    const response = await fetch(api);
+    const data = await response.json();
+    response_headers["content-type"] = "application/json";
     return new Response(JSON.stringify(data), {
-      headers: {
-        "content-type": "application/json",
-        // "cache-control": "max-age=60"
-      }
-    })
-
+      status: 200,
+      headers: response_headers
+    });
+  },
+  v2: async (parts,env)=>{
+    const route = parts[0];
+    const section = parts[1];
+    const id = parts[2];
+    const selections = parts[3];
+    if (!section) {
+      return new Response([
+        `Use /${route}/{section}/${id}/${selections}`
+      ].join('\n'), { status: 400 });
+    } else if (!id) {
+      return new Response([
+        `Use /${route}/${section}/{id}/{selections}`
+      ].join('\n'), { status: 400 });
+    }
+    return new Response([
+      `Use /${route}/${section}/${id}/${selections}`
+    ].join('\n'), { status: 400 });
   }
-}
+};
+var worker_default = {
+  async fetch(request,env) {
+    const url = new URL(request.url);
+    const parts = url.pathname.split("/").filter(Boolean);
+    const route = parts[0];
+    if (response[route]) {
+      return response[route](parts,env);
+    }
+    else {
+      return response.help();
+    }
+  }
+};
+export {
+  worker_default as default
+};
+//# sourceMappingURL=worker.js.map
